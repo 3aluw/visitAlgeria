5d3ff1beb7b5aef2624d439b0db7467acab7f9d2
Abdou <2000abdallah2014@gmail.com>
2022-12-27 21:39:11 +0100

    added transition to fourth sections + show more button

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,61 @@
         <ImagesCarousel />
         <AlgeriaFeatures />
         <BestPlaces />
-        <Cities />
+        <div class="cities-button-cont">
+            <button @click="toggleCitites" v-if="!showCitites">Show more</button>
+        </div>
+        <Transition name="cities">
+            <Cities v-if="showCitites" />
+        </Transition>
         <FAQ></FAQ>
     </div>
 </template>
+<script setup>
+const showCitites = ref(false);
+const toggleCitites = () => {
+    showCitites.value = !showCitites.value;
+}
 
+</script>
 <style>
 body {
     margin: 0;
+    background: rgba(228, 240, 255, 0.42);
+
+}
+
+
+.cities-button-cont {
+    display: flex;
+    justify-content: center;
+    padding-block: 2rem;
+    background: rgba(228, 240, 255, 0.42);
+}
+
+.cities-button-cont>button {
+    box-sizing: border-box;
+    cursor: pointer;
+    background: rgb(0, 180, 51, 0.5);
+    color: #ffffff;
+    font-family: 'Outfit';
+    font-size: 2rem;
+    font-weight: 400;
+    -webkit-text-stroke: 0.19px rgb(255, 255, 255);
+    border-radius: 5px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    border: 1px black solid;
+    padding-inline: 1rem;
+}
+
+.cities-enter-active,
+.cities-leave-active {
+    transition: all 1s ease;
+}
+
+.cities-enter-from,
+.cities-leave-to {
+    opacity: 0;
+    transform: translateX(-100%);
 }
 </style>
   
