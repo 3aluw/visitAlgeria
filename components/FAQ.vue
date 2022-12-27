0733ee14d839368cc7a1294dce8f8018b2ecea7c
Abdou <2000abdallah2014@gmail.com>
2022-12-27 21:09:47 +0100

    redesigned section 2 + minor updates on FAQ

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -25,19 +25,19 @@
                     <v-expansion-panel-text>
                         <p>The cost of living in Algeria is, on average, 61.76% lower than in United States,the exchange
                             rate has also been favorable for tourists, so it is possible to buy goods at a very low
-                            price. you can see prices : <a target="_blank" rel="noopener noreferrer"
+                            price. you can check prices <a target="_blank" rel="noopener noreferrer"
                                 href="https://www.numbeo.com/cost-of-living/country_result.jsp?country=Algeria&displayCurrency=USD">here</a>
 
                         </p>
                     </v-expansion-panel-text>
                 </v-expansion-panel>
                 <v-expansion-panel title="How can i plan my trip ?"
-                    text="A traveling agency acan help you, this site can too!">
+                    text="A traveling agency can help you, this site can too!">
                 </v-expansion-panel>
                 <v-expansion-panel title="is visa required ?">
                     <v-expansion-panel-text>
                         <p>That dependes on your home country, you can check this <a rel="noopener noreferrer"
-                                href="https://www.markuslerner.com/travelscope/">interactive map</a>
+                                target="_blank" href="https://www.markuslerner.com/travelscope/">interactive map</a>
 
                         </p>
                     </v-expansion-panel-text>
@@ -45,15 +45,25 @@
                 </v-expansion-panel>
             </v-expansion-panels>
         </div>
-
+        <footer>
+            <h1>Proudly created by <span><a href="https://twitter.com/3aluwa" rel="noopener noreferrer"
+                        target="_blank">@3aluw</a></span></h1>
+            <div class="links">
+                <a href="https://github.com/3aluw/visitAlgeria" rel="noopener noreferrer" target="_blank">Give it a
+                    start</a><a href="https://github.com/3aluw/visitAlgeria" rel="noopener noreferrer"
+                    target="_blank">Contact me</a><a href="https://ko-fi.com/3aluw" rel="noopener noreferrer"
+                    target="_blank">Support me</a>
+            </div>
+        </footer>
     </div>
+
 </template>
 <script setup>
 
 </script>
 <style scoped>
 .cont {
-    padding-block: 2rem;
+    padding-block-start: 2rem;
     min-height: 100vh;
     display: grid;
     grid-template-rows: 10% 1fr;
@@ -87,5 +97,40 @@
 
 .questions-panel a {
     color: #1f914c;
+}
+
+footer {
+    background-color: #16251C;
+    padding: 2rem;
+    color: white;
+}
+
+footer>h1 {
+    text-align: center;
+    font-size: 1.5rem;
+    font-family: 'Century Gothic';
+    font-style: normal;
+    font-weight: 400;
+
+
+}
+
+footer>h1>span {
+    color: #1D9BF0;
+}
+
+footer>.links {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    padding-block: 1rem;
+}
+
+.links a {
+    min-width: 200px;
+}
+
+.links a:hover {
+    color: #00FF67;
 }
 </style>
